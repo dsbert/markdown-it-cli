939e3d29916cf572f78711bda499332fce9512c0
dsbert <danbertolini@gmail.com>
2018-03-25 16:22:39 -0400

    Unit test for default option loading

--- test/index.js
+++ test/index.js
@@ -1,9 +1,15 @@
 const assert = require("assert");
 const sinon = require("sinon");
-const options = require("../lib/options");
+const loadOptions = require("../lib/options").load;
 
 describe("options", function() {
-  it("should report an error if path is not found", () => {
+  xit("should report an error if path is not found", () => {
     options.load("does not exist");
+  });
+
+  it("should load a local .markdownrc.json by default", () => {
+    const { options, plugins } = loadOptions();
+    assert(plugins.length > 0, "Should load plugins");
+    assert(Object.prototype.hasOwnProperty.apply(options, ["html"]));
   });
 });
